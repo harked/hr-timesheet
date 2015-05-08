54e0da4c3713edc5ad54452d3bf3381cd7054387
Alexander Voitenko <voitenko@gmail.com>
2015-05-08 11:18:19 +0300

    Changed function to calculate duty hours with leavs hours

--- hr_timesheet_dh/models.py
+++ hr_timesheet_dh/models.py
@@ -63,14 +63,15 @@ class hr_timesheet_dh(osv.osv):
             dates = list(rrule.rrule(rrule.DAILY,
                                      dtstart=parser.parse(sheet.date_from),
                                      until=parser.parse(sheet.date_to)))
+            leave_intervals = self.get_leave_intervals(cr, uid, sheet.employee_id.id)
             for date_line in dates:
-                duty_hours = self.calculate_duty_hours(cr, uid, sheet.employee_id.id, date_line, context)
+                duty_hours = self.calculate_duty_hours(cr, uid, sheet.employee_id.id, date_line, leave_intervals, context)
                 self.pool.get('hr_timesheet.day.dh').create(cr,
                                                             uid,
                                                             {'name': date_line, 'duty_hours': duty_hours, 'sheet_id': sheet.id},
                                                             context)
 
-    def calculate_duty_hours(self, cr, uid, employee_id, date_from, context):
+    def calculate_duty_hours(self, cr, uid, employee_id, date_from, leave_intervals, context):
         contract_obj = self.pool.get('hr.contract')
         calendar_obj = self.pool.get('resource.calendar')
         duty_hours = 0.0
@@ -81,9 +82,21 @@ class hr_timesheet_dh(osv.osv):
             dh = calendar_obj.get_working_hours_of_date(cr=cr, uid=uid,
                                                          id=contract.working_hours.id,
                                                          start_dt=date_from,
+                                                         leaves=leave_intervals,
                                                          context=context)
             duty_hours += dh
         return duty_hours
+
+    def get_leave_intervals(self, cr, uid, employee_id):
+        intervals = []
+        holiday_obj = self.pool.get('hr.holidays')
+        holidays = holiday_obj.search_read(cr, uid,
+                                           [('state', '=', 'validate'), ('employee_id', '=', employee_id),],
+                                           ['date_from', 'date_to']
+                                           )
+        for holiday in holidays:
+            intervals.append((parser.parse(holiday['date_from']), parser.parse(holiday['date_to'])))
+        return intervals
 
     def get_previous_month_diff(self, cr, uid, employee_id, prev_timesheet_date_from, context=None):
         total_diff = 0.0
@@ -165,7 +178,40 @@ class hr_timesheet_day_dh(osv.osv):
         self.duty_hours = 5.0
 
     _columns = {
-        'sheet_id': fields.many2one('hr_timesheet_sheet.sheet', 'Sheet'),
+        'sheet_id': fields.many2one('hr_timesheet_sheet.sheet', 'Sheet', ondelete='cascade'),
         'name': fields.date('Date'),
         'duty_hours': fields.float('Duty Hours'),
     }
+
+class hr_holidays_dh(osv.osv):
+    _inherit = 'hr.holidays'
+
+    def write(self, cr, uid, ids, vals, context=None):
+        res = super(hr_holidays_dh, self).write(cr, uid, ids, vals, context=context)
+        if not res: return res
+        if 'state' in vals and vals['state'] in ['validate', 'refuse']:
+            for holiday in self.browse(cr, uid, ids, context):
+                timesheet_ids = self.get_timesheet_ids(cr, uid, holiday)
+                if not timesheet_ids: return res
+                dh_ids = self.get_duty_hour_ids(cr, uid, timesheet_ids[0], holiday, context)
+                leave_intervals = self.pool.get('hr_timesheet_sheet.sheet').get_leave_intervals(cr, uid, holiday.employee_id.id)
+                self.update_duty_hours(cr, uid, dh_ids, holiday.employee_id.id, leave_intervals, context)
+        return res
+
+    def get_timesheet_ids(self, cr, uid, holiday, context=None):
+        return self.pool.get('hr_timesheet_sheet.sheet').search(cr, uid,
+                [('employee_id', '=', holiday.employee_id.id),
+                ('date_from', '<=', holiday.date_from),
+                ('date_to', '>=', holiday.date_to)], context)
+
+    def get_duty_hour_ids(self, cr, uid, timesheet_id, holiday, context=None):
+        return self.pool.get('hr_timesheet.day.dh').search(cr, uid,
+                        [('sheet_id', '=', timesheet_id),
+                        ('name', '>=', holiday.date_from),
+                        ('name', '<=', holiday.date_to)], context)
+
+    def update_duty_hours(self, cr, uid, dh_ids, employee_id, leave_intervals, context):
+        for dh in self.pool.get('hr_timesheet.day.dh').browse(cr, uid, dh_ids):
+            hours = self.pool.get('hr_timesheet_sheet.sheet')\
+                .calculate_duty_hours(cr, uid, employee_id, parser.parse(dh.name), leave_intervals, context)
+            dh.write({'duty_hours': hours})
